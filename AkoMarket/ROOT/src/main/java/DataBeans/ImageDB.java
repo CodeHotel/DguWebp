825a3173dcb48247d579e317547889392b3df0a7
Jeong <89632518+CodeHotel@users.noreply.github.com>
2023-12-18 19:26:35 +0900

    ImgTest2

--- AkoMarket/ROOT/src/main/java/DataBeans/ImageDB.java
+++ AkoMarket/ROOT/src/main/java/DataBeans/ImageDB.java
@@ -1,12 +1,16 @@
 package DataBeans;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 public class ImageDB {
-    private static final String DB_URL = "172.31.1.245:2580";
+    private static final String DB_URL = "localhost:2580";
+    private static final String UPLOAD_DIRECTORY = "/usr/local/Server_Repo/ImageDB/";
     private static final String KEY = "akoImage";
 
     // Method to get the image URL
@@ -36,5 +40,35 @@ public class ImageDB {
         }
 
         return response.toString();
+    }
+
+    public static String uploadFile(Part filePart, String masterKeyParam) throws IOException {
+        // Check master key
+        if (!KEY.equals(masterKeyParam)) {
+            throw new IOException("Wrong master key");
+        }
+
+        String fileName = "NULL";
+        try (InputStream fileContent = filePart.getInputStream();
+             FileOutputStream out = new FileOutputStream(new File(UPLOAD_DIRECTORY, fileName))) {
+
+            String submittedFileName = filePart.getSubmittedFileName();
+            String fileExtension = submittedFileName.substring(submittedFileName.lastIndexOf("."));
+            fileName = UUID.randomUUID().toString() + fileExtension; // Manual string for filename
+
+            File uploadDir = new File(UPLOAD_DIRECTORY);
+            if (!uploadDir.exists()) uploadDir.mkdir(); // Create upload directory if it doesn't exist
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fileContent.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            // Handle exceptions
+            e.printStackTrace();
+            throw e; // Rethrow to indicate failure
+        }
+        return fileName;
     }
 }
