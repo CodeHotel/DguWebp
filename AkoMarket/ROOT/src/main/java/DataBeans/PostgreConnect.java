875f5911824af89eab527f7f7eaf9772206956ac
Jeong <89632518+CodeHotel@users.noreply.github.com>
2023-12-18 17:38:46 +0900

    Login In

--- AkoMarket/ROOT/src/main/java/DataBeans/PostgreConnect.java
+++ AkoMarket/ROOT/src/main/java/DataBeans/PostgreConnect.java
@@ -6,14 +6,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PostgreConnect {
-    private static final String DB_URL = "jdbc:mysql://akodb.cwmg6nnupeuw.ap-northeast-2.rds.amazonaws.com/AkoDB";
+    private static final String DB_URL = "jdbc:postgresql://akodb.cwmg6nnupeuw.ap-northeast-2.rds.amazonaws.com/AkoDB";
     private static final String DB_USER = "akomarket";
     private static final String DB_PASSWORD = "akomarket";
 
     static {
         try {
             System.out.println("Loading JDBC driver...");
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.postgresql.Driver");
             System.out.println("JDBC driver loaded successfully.");
         } catch (ClassNotFoundException e) {
             System.out.println("Failed to load JDBC driver.");
