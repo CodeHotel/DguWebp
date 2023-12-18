27bb231fdcb72f768eef965665e10ec5783ec934
ross1573 <ross15733578@gmail.com>
2023-12-19 04:03:34 +0900

    fix DB

--- AkoMarket/ROOT/src/main/java/DataBeans/Wishlist.java
+++ AkoMarket/ROOT/src/main/java/DataBeans/Wishlist.java
@@ -3,11 +3,9 @@ package DataBeans;
 public class Wishlist {
     Product product;
     User user;
-    Progress progress;
 
     public Wishlist(Product product, User user, Progress progress) {
         this.product = product;
         this.user = user;
-        this.progress = progress;
     }
 }
